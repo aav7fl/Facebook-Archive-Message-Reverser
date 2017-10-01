f47002c7450019a173aede77e0c7e640cf44a1fa
Kyle Niewiada <aav7fl@users.noreply.github.com>
2017-10-01 09:14:51 -0400

    Fix comments. They are ugly.

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@
 # File name: parser.py             #
 # Author: Kyle Niewiada, anonymous #
 # Date created: 2015, October 09   #
-# Date modified: 2015, October 17  #
+# Date modified: 2017, October 01  #
 # Python version: 2.7              #
 ####################################
 from bs4 import BeautifulSoup
@@ -11,25 +11,25 @@ import sys
 def reverse_messages(soup):
     divOfThreads = list(soup.select('.contents > div'))
 
-    """for each group of divs containing <div class="thread">"""
+    #for each group of divs containing <div class="thread">
     for div in range(0, len(divOfThreads)):
 
-        """for each thread of messages inside each div"""        
+        #for each thread of messages inside each div
         for threadDiv in range (0, len(divOfThreads[div].select('.thread'))):
         
-            """select all the messages inside each thread"""
+            #select all the messages inside each thread
             p = list(divOfThreads[div].select('.thread')[threadDiv])[::-1]
             
-            """encode to UTF-8 to prevent Unicode errors"""
+            #encode to UTF-8 to prevent Unicode errors
             q = [x.encode('utf-8') for x in p]
 
             length = len(q)
 
-            """now reverse all message pairs inside the current thread"""
+            #now reverse all message pairs inside the current thread
             for x in range(0, length-1, 2):
                 q[x], q[x+1] = q[x+1], q[x]
 
-            """append the reversed message order to the messages_reversed.html file"""
+            #append the reversed message order to the messages_reversed.html file
             reversedHTMLFile = open('messages_reversed.html', 'a')
             reversedHTMLFile.write('\n'.join(q))
             reversedHTMLFile.close()
@@ -40,14 +40,14 @@ if __name__ == "__main__":
         print("USAGE: %s [HTML FILE]" % sys.argv[0])
         sys.exit(1)
     
-	"""parse the HTML file from the command line argument with BeautifulSoup"""	
+    #parse the HTML file from the command line argument with BeautifulSoup
     soup = BeautifulSoup(open(sys.argv[1]), "html.parser")
     
-    """re-create messages_reversed.html file for writing"""
+    #re-create messages_reversed.html file for writing
     reversedHTMLFile = open('messages_reversed.html', 'w')
     reversedHTMLFile.close()
     
-    """pass the parsed file to reverse the messages"""
+    #pass the parsed file to reverse the messages
     reverse_messages(soup)
 
     print("Done")
